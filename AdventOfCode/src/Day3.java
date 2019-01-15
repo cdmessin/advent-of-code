42518fd08ebde1e3962abe010c40a3377116e68f
cdmessin <cdmessin@ncsu.edu>
2019-01-15 15:25:28 -0500

    Day 3 Part One working

--- AdventOfCode/src/Day3.java
+++ AdventOfCode/src/Day3.java
@@ -18,30 +18,45 @@ public class Day3 {
 			processLine(s.nextLine(), fabric);
 		}
 		System.out.println(duplicateCount);
+		//130452 is too high.
 		s.close();
 	}
 	
 	private static void processLine(String line, int[][] fabric) {
 		Scanner lineScanner = new Scanner(line);
 		//Gets rid of ID
-		lineScanner.nextInt();
-		int startRow = lineScanner.nextInt();
-		int startColumn = lineScanner.nextInt();
-		int endColumn = startColumn + lineScanner.nextInt();
-		int endRow = startRow + lineScanner.nextInt();
-		
-		for(int i = startRow; i <= endRow; i++) {
-			for(int j = startColumn; j <= endColumn; j++) {
+		lineScanner.next();
+		//Removes the @ sign
+		lineScanner.next();
+		lineScanner.useDelimiter(",");
+		int startColumn = Integer.parseInt(lineScanner.next().trim());
+		lineScanner.useDelimiter(":");
+		String secondNumber = lineScanner.next();
+		int startRow = Integer.parseInt(secondNumber .substring(1, secondNumber.length()));
+		lineScanner.useDelimiter(" ");
+		//gets rid of anything before the next number
+		lineScanner.next();
+		lineScanner.useDelimiter("x");
+		int endColumn = startColumn + Integer.parseInt(lineScanner.next().trim());
+		int endRow = startRow + Integer.parseInt(lineScanner.next().trim());
+//		System.out.println("Start Column: " + startColumn);
+//		System.out.println(startRow);
+//		System.out.println(endColumn);
+//		System.out.println(endRow);
+
+		for(int i = startRow; i <= endRow - 1; i++) {
+			for(int j = startColumn; j <= endColumn - 1; j++) {
+				boolean notFirst = true;
 				if(fabric[i][j] == 0) {
 					fabric[i][j] = 1;
-				} else if (fabric[i][j] == 1){
+					notFirst = false;
+				} else if (fabric[i][j] == 1 && notFirst){
 					duplicateCount++;
 					fabric[i][j] = fabric[i][j] + 1;
 				} else {
 					fabric[i][j] = fabric[i][j] + 1;
 				}
-				
-			}
+			}			
 		}
 		lineScanner.close();
 	}
