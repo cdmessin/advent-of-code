054420ec58603003c99d2a67a79acd05e171ece6
cdmessin <cdmessin@ncsu.edu>
2019-01-17 14:12:06 -0500

    Day 3 Completed

--- AdventOfCode/src/Day3.java
+++ AdventOfCode/src/Day3.java
@@ -18,7 +18,17 @@ public class Day3 {
 			processLine(s.nextLine(), fabric);
 		}
 		System.out.println(duplicateCount);
-		//130452 is too high.
+		s.close();
+		
+		//Checks for duplicate for each line
+		try {
+			s = new Scanner(new File("test-files/input-3.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(s.hasNextLine()) {
+			checkDuplicate(s.nextLine(), fabric);
+		}
 		s.close();
 	}
 	
@@ -43,7 +53,7 @@ public class Day3 {
 //		System.out.println(startRow);
 //		System.out.println(endColumn);
 //		System.out.println(endRow);
-
+		boolean overlap = false;
 		for(int i = startRow; i <= endRow - 1; i++) {
 			for(int j = startColumn; j <= endColumn - 1; j++) {
 				boolean notFirst = true;
@@ -53,10 +63,47 @@ public class Day3 {
 				} else if (fabric[i][j] == 1 && notFirst){
 					duplicateCount++;
 					fabric[i][j] = fabric[i][j] + 1;
+					overlap = true;
 				} else {
 					fabric[i][j] = fabric[i][j] + 1;
+					overlap = true;
 				}
-			}			
+			}	
+		}
+		lineScanner.close();
+	}
+	
+	private static void checkDuplicate(String line, int[][] fabric) {
+		Scanner lineScanner = new Scanner(line);
+		//Gets rid of ID
+		lineScanner.next();
+		//Removes the @ sign
+		lineScanner.next();
+		lineScanner.useDelimiter(",");
+		int startColumn = Integer.parseInt(lineScanner.next().trim());
+		lineScanner.useDelimiter(":");
+		String secondNumber = lineScanner.next();
+		int startRow = Integer.parseInt(secondNumber .substring(1, secondNumber.length()));
+		lineScanner.useDelimiter(" ");
+		//gets rid of anything before the next number
+		lineScanner.next();
+		lineScanner.useDelimiter("x");
+		int endColumn = startColumn + Integer.parseInt(lineScanner.next().trim());
+		int endRow = startRow + Integer.parseInt(lineScanner.next().trim());
+//		System.out.println("Start Column: " + startColumn);
+//		System.out.println(startRow);
+//		System.out.println(endColumn);
+//		System.out.println(endRow);
+		boolean overlap = false;
+		for(int i = startRow; i <= endRow - 1; i++) {
+			for(int j = startColumn; j <= endColumn - 1; j++) {
+				if(fabric[i][j] > 1) {
+					overlap = true;
+				}
+			}	
+		}
+		if(!overlap) {
+			System.out.println(line);
 		}
 		lineScanner.close();
 	}
